c1b9377db35f22717451bba6d85c87d7d15366ac
OldSwade <133116353+OldSwade@users.noreply.github.com>
2024-07-09 11:19:24 -0300

    C2
    09/07/24 (DD/MM/YY) - C2
    
    Wifi scanner - Arduino 
    
    Wrong file was added.

--- wifi_scan2.4.ino
+++ wifi_scan2.4.ino
@@ -1,145 +1,155 @@
+
+
+/*
+Author : Morgan Mootoosamy
+Date : 06/05/2024 (MM/DD/YYYY)
+Updated : 07/09/2024 (MM/DD/YYYY)
+Title : WiFi 2.4 GHz scanner
+File : wifi_scan2.4.ino
+*/
+
+
+
+
 #include "WiFi.h"
-//#include <TinyGPS++.h>
-#include <SoftwareSerial.h>
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
 
-//TinyGPSPlus gps;
+TinyGPSPlus gps;
 
-//static const int RXPin = 12, TXPin = 34;
-//static const uint32_t GPSBaud = 4800;
 
-float latitude = -1;
-float longitude = -1;
-float altitude = -1;
-
-//SoftwareSerial ss(RXPin, TXPin);
+HardwareSerial gpsserial(1);  
 
 void setup()
 {
     Serial.begin(115200);
-    //ss.begin(GPSBaud);
+    gpsserial.begin(9600, SERIAL_8N1, 34, 12);;
     // Set WiFi to station mode and disconnect from an AP if it was previously connected.
     WiFi.mode(WIFI_STA);
     WiFi.disconnect();
     delay(10);
     
 }
+
+static void smartDelay(unsigned long ms)                
+{
+  unsigned long start = millis();
+  do
+  {
+    while (gpsserial.available())
+      gps.encode(gpsserial.read());
+  } while (millis() - start < ms);
+}
   
 void loop()
-{     
-      /*while (ss.available() > 0){
-        //Serial.print(ss.read());
-        //Serial.print(gps.encode(ss.read()));
-        //Serial.print(gps.charsProcessed());
-        //Serial.print(gps.location.isValid());
-        if (gps.encode(ss.read()))
-        {
-          
-          if (gps.location.isValid())
-          {
-            float latitude = (gps.location.lat(),6);
-            float longitude = (gps.location.lng(),6);
+{
+       
+
+  gps.location.lat(), 6;
+  gps.location.lng(), 6;
+  smartDelay(250);                                      
+  
+  int n = WiFi.scanNetworks();
+  delay(50);
+  
+  if (n == 0) {
+    Serial.print("[");
+    Serial.print("{ \"id\" :\"");
+    Serial.print("1");
+    Serial.print("\", \"Lat\" :\"");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print("\", \"Long\" :\"");
+    Serial.print(gps.location.lng(), 6);
+    smartDelay(250); 
+    Serial.print("\", \"SSID\" :\"");
+    Serial.print("None");
+    Serial.print("\", \"RSSI\" :\"");
+    Serial.print("-999");
+    Serial.print("\", \"CH\" :\"");
+    Serial.print("-1");
+    Serial.print("\", \"Enc\" :\"");
+    Serial.print("None");
+    Serial.print("\", \"BSSID\" :\"");
+    Serial.print("None\"}");
+  } else { 
+    Serial.print("[");
+      for (int i = 0; i < n; ++i) {
+          Serial.print("{ \"id\" :\"");
+          Serial.print(i+1);
+          Serial.print("\", \"Lat\" :\"");
+          if (i == 0) {
+            Serial.print(gps.location.lat(), 6);
           }
-          
-        }
-          
-      if (millis() > 5000 && gps.charsProcessed() < 10)
-      {
-        while(true);
-      }
-    }
-    Serial.print("Latitude : ");
-    Serial.print(gps.location.lat(),6);
-    Serial.println("");
-    Serial.print("Longitude : ");
-    Serial.print(gps.location.lng(),6);
-    Serial.println("");
-    */
-
-    // WiFi.scanNetworks will return the number of networks found.
-
-    int n = WiFi.scanNetworks();
-    delay(50);
-    
-    if (n == 0) {
-      Serial.print("[");
-      Serial.print("{ \"id\" :\"");
-      Serial.print("None");
-      Serial.print("\", \"Lat\" :\"");
-      Serial.print(latitude);
-      Serial.print("\", \"Long\" :\"");
-      Serial.print(longitude);
-      Serial.print("\", \"SSID\" :\"");
-      Serial.print("None");
-      Serial.print("\", \"RSSI\" :\"");
-      Serial.print("None");
-      Serial.print("\", \"CH\" :\"");
-      Serial.print("None");
-      Serial.print("\", \"Enc\" :\"");
-      Serial.print("None");
-      Serial.print("\", \"BSSID\" :\"");
-      Serial.print("None\"}");
-    } else { 
-      Serial.print("[");
-        for (int i = 0; i < n; ++i) {
-            Serial.print("{ \"id\" :\"");
-            Serial.print(i+1);
-            Serial.print("\", \"Lat\" :\"");
-            Serial.print(latitude);
-            Serial.print("\", \"Long\" :\"");
-            Serial.print(longitude);
-            Serial.print("\", \"SSID\" :\"");
-            Serial.print(WiFi.SSID(i).c_str());
-            Serial.print("\", \"RSSI\" :\"");
-            Serial.print(WiFi.RSSI(i));
-            Serial.print("\", \"CH\" :\"");
-            Serial.print(WiFi.channel(i));
-            Serial.print("\", \"Enc\" :\"");
-            switch (WiFi.encryptionType(i))
-            {
-            case WIFI_AUTH_OPEN:
-                Serial.print("open\"");
-                break;
-            case WIFI_AUTH_WEP:
-                Serial.print("WEP\"");
-                break;
-            case WIFI_AUTH_WPA_PSK:
-                Serial.print("WPA\"");
-                break;
-            case WIFI_AUTH_WPA2_PSK:
-                Serial.print("WPA2\"");
-                break;
-            case WIFI_AUTH_WPA_WPA2_PSK:
-                Serial.print("WPA+WPA2\"");
-                break;
-            case WIFI_AUTH_WPA2_ENTERPRISE:
-                Serial.print("WPA2-EAP\"");
-                break;
-            case WIFI_AUTH_WPA3_PSK:
-                Serial.print("WPA3\"");
-                break;
-            case WIFI_AUTH_WPA2_WPA3_PSK:
-                Serial.print("WPA2+WPA3\"");
-                break;
-            case WIFI_AUTH_WAPI_PSK:
-                Serial.print("WAPI\"");
-                break;
-            default:
-                Serial.print("unknown\"");
-            }
-            Serial.print(", \"BSSID\" :\"");
-            Serial.print(WiFi.BSSIDstr(i));
-            if (i+1 == n){
-              Serial.print("\"}");
+          else{
+            Serial.print("REPLACE");
+          }
+          Serial.print("\", \"Long\" :\"");
+          if (i == 0) {
+            Serial.print(gps.location.lng(), 6);
+            smartDelay(250); 
+          }
+          else{
+            Serial.print("REPLACE");
+          }
+          Serial.print("\", \"SSID\" :\"");
+            if (WiFi.SSID(i).c_str() == ""){
+              Serial.print("Unknown");
             }
             else{
-              Serial.print("\"},");
+              Serial.print(WiFi.SSID(i).c_str());
             }
-            delay(10);
-        }
-    }
-    Serial.print("],");
+          Serial.print("\", \"RSSI\" :\"");
+          Serial.print(WiFi.RSSI(i));
+          Serial.print("\", \"CH\" :\"");
+          Serial.print(WiFi.channel(i));
+          Serial.print("\", \"Enc\" :\"");
+          switch (WiFi.encryptionType(i))
+          {
+          case WIFI_AUTH_OPEN:
+              Serial.print("open\"");
+              break;
+          case WIFI_AUTH_WEP:
+              Serial.print("WEP\"");
+              break;
+          case WIFI_AUTH_WPA_PSK:
+              Serial.print("WPA\"");
+              break;
+          case WIFI_AUTH_WPA2_PSK:
+              Serial.print("WPA2\"");
+              break;
+          case WIFI_AUTH_WPA_WPA2_PSK:
+              Serial.print("WPA/WPA2\"");
+              break;
+          case WIFI_AUTH_WPA2_ENTERPRISE:
+              Serial.print("WPA2-EAP\"");
+              break;
+          case WIFI_AUTH_WPA3_PSK:
+              Serial.print("WPA3\"");
+              break;
+          case WIFI_AUTH_WPA2_WPA3_PSK:
+              Serial.print("WPA2/WPA3\"");
+              break;
+          case WIFI_AUTH_WAPI_PSK:
+              Serial.print("WAPI\"");
+              break;
+          default:
+              Serial.print("unknown\"");
+          }
+          Serial.print(", \"BSSID\" :\"");
+          Serial.print(WiFi.BSSIDstr(i));
+          if (i+1 == n){
+            Serial.print("\"}");
+          }
+          else{
+            Serial.print("\"},");
+          }
+          delay(10);
+      }
+  }
+    Serial.print("],+");
     Serial.println("");
  
     // Delete the scan result to free memory for code below.
     WiFi.scanDelete(); 
 }
+
